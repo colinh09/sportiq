5a87b54f527dbea1e56077b9d94f7abbeee45f57
wmontagu <wmontagu@andrew.cmu.edu>
2024-12-18 18:06:28 -0600

    an api pull request from nba source

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+import requests
+import json
+
+def get_nba_scores():
+    url = "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
+
+
+def print_scores(data):
+    if data and 'season' in data:
+        for game in data['season']:
+            print(json.dumps(game, indent=2))
+    print(data.keys())
+
+
+scores = get_nba_scores()
+print_scores(scores)
